62c31fcb06497407d642c8ed5b2bfd7b876b49d6
Victor Sibanda <victormsibanda1@gmail.com>
2020-04-17 16:06:15 +0100

    Bash Install Chef

--- Cookbooks/ChefPackerAMICookbook/recipes/default.rb
+++ Cookbooks/ChefPackerAMICookbook/recipes/default.rb
@@ -14,4 +14,10 @@ end
 
 package 'packer'
 
-package 'chef'
+bash 'install chef' do
+  code <<-EOL
+  wget  https://packages.chef.io/files/stable/chef-workstation/0.2.43/ubuntu/18.04/chef-workstation_0.2.43-1_amd64.deb
+  sudo dpkg -i chef-workstation_*.deb
+  rm chef-workstation_*.deb
+  EOL
+end
